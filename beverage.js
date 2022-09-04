e9e7a42622488b2d9b82637148de1a28214abb66
JeonSeoHee <78894678+SeoHee3478@users.noreply.github.com>
2022-09-04 22:50:08 +0900

    docs: 판매할 음료 데이터 파일 추가
    beverage.js에 음료에 대한 name, price, image 에 대한 데이터 파일 추가

---
+++ beverage.js
@@ -0,0 +1,32 @@
+export default [
+    {
+    "name":"Original Cola",
+    "price":"1000원",
+    "image":".src/images/Original_Cola.png"
+    },
+    {
+      "name": "Violet Cola",
+      "price": "1000원",
+      "image": ".src/images/Violet_Cola.png"
+    },
+    {
+      "name": "Yellow Cola",
+      "price": "1000원",
+      "image": ".src/images/Yellow_Cola.png"
+    },
+    {
+      "name": "Cool Cola",
+      "price": "1000원",
+      "image": ".src/images/Cool_Cola.png"
+    },
+    {
+      "name": "Green Cola",
+      "price": "1000원",
+      "image": ".src/images/Green_Cola.png"
+    },
+    {
+      "name": "Orange Cola",
+      "price": "1000원",
+      "image": ".src/images/Orange_Cola.png"
+    }
+]
