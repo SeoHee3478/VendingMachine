8b7947b099f6eeac4c55738921615e94b58988a0
JeonSeoHee <78894678+SeoHee3478@users.noreply.github.com>
2022-09-05 11:56:04 +0900

    refactor: beverage.js src 경로 수정

--- beverage.js
+++ beverage.js
@@ -2,31 +2,31 @@ export default [
     {
     "name":"Original Cola",
     "price":"1000원",
-    "image":".src/images/Original_Cola.png"
+    "image":"./src/images/Original_Cola.png"
     },
     {
       "name": "Violet Cola",
       "price": "1000원",
-      "image": ".src/images/Violet_Cola.png"
+      "image": "./src/images/Violet_Cola.png"
     },
     {
       "name": "Yellow Cola",
       "price": "1000원",
-      "image": ".src/images/Yellow_Cola.png"
+      "image": "./src/images/Yellow_Cola.png"
     },
     {
       "name": "Cool Cola",
       "price": "1000원",
-      "image": ".src/images/Cool_Cola.png"
+      "image": "./src/images/Cool_Cola.png"
     },
     {
       "name": "Green Cola",
       "price": "1000원",
-      "image": ".src/images/Green_Cola.png"
+      "image": "./src/images/Green_Cola.png"
     },
     {
       "name": "Orange Cola",
       "price": "1000원",
-      "image": ".src/images/Orange_Cola.png"
+      "image": "./src/images/Orange_Cola.png"
     }
 ]
