02da783bd8470b0a8ff1c8cd000fd3f65a4490fb
JeonSeoHee <78894678+SeoHee3478@users.noreply.github.com>
2022-09-05 11:58:49 +0900

    feat: beverage.js에 저장된 데이터를 동적으로 가져오도록 수정
    createElement를 이용하여 노드를 생성하고, 해당하는 src와 text들은 beverage.js에 저장된 값들로 넣어줌

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+import beverage from './beverage.js';
 //상품 정보 데이터 불러오기
 import beverageList from './beverage.js';
 
+const listItem = document.querySelector('.list-item');
 //음료수 별 클릭 횟수 배열로 담기
 
 //상품 갯수만큼 상품 버튼 생성
 
 //음료 클릭 시 장바구니에 담기는 기능 구현
+beverageList.forEach(item=>{
+    const button = document.createElement('button');
+    button.className="btn-item";
 
+    const image = document.createElement('img'); //image라고 해서 계속 image 가 반영 안됨
+    image.className = "img-item";
+    image.src = item.image;
+
+    const productName = document.createElement('strong');
+    productName.className = "tit-item";
+    productName.innerText = item.name;
+
+    const productPrice = document.createElement('span');
+    productPrice.className = "txt-price";
+    productPrice.innerText = item.price;
+
+    listItem.appendChild(button);
+    button.append(image,productName,productPrice);
+
+});
+
+console.log(beverageList);
